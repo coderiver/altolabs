01ba7485c770c8e446f3afe3484145f5cdfdf287
buschidos <sergey.snjozyk@gmail.com>
2015-12-17 13:57:36 +0200

    upd projects animations

--- app/js/modules/animations-base.js
+++ app/js/modules/animations-base.js
@@ -1,6 +1,6 @@
 export function createHeadingAnimFor(context) {
     let heading = context.find('.project__heading');
-    let ease    = Power1.easeInOut;
+    let ease    = Power3.easeOut;
 
     TweenMax.set(heading, { y: -220, autoAlpha: 0 });
 
@@ -18,30 +18,30 @@ export function createAboutAnimFor(context) {
     let btn         = context.find('.project__about .btn');
     let ease        = Power3.easeOut;
 
-    TweenMax.set([title, line, description, btn], { x: -200, autoAlpha: 0 });
+    TweenMax.set([title, line, description, btn], { x: -150, autoAlpha: 0 });
 
     return [
-        TweenMax.to(title, 0.8, {
+        TweenMax.to(title, 0.5, {
             x: 0,
             autoAlpha: 1,
             ease
         }),
-        TweenMax.to(line, 0.8, {
+        TweenMax.to(line, 0.5, {
+            x: 0,
+            autoAlpha: 1,
+            delay: 0.1,
+            ease
+        }),
+        TweenMax.to(description, 0.5, {
+            x: 0,
+            autoAlpha: 1,
+            delay: 0.2,
+            ease
+        }),
+        TweenMax.to(btn, 0.5, {
             x: 0,
             autoAlpha: 1,
             delay: 0.25,
-            ease
-        }),
-        TweenMax.to(description, 0.8, {
-            x: 0,
-            autoAlpha: 1,
-            delay: 0.5,
-            ease
-        }),
-        TweenMax.to(btn, 0.8, {
-            x: 0,
-            autoAlpha: 1,
-            delay: 0.6,
             ease
         })
     ];
--- app/js/modules/animations.js
+++ app/js/modules/animations.js
@@ -8,38 +8,30 @@ const qmedic    = new TimelineMax({ paused: true });
 const milkyway  = new TimelineMax({ paused: true });
 const rexpro    = new TimelineMax({ paused: true });
 
-const ease      = Power2.easeOut;
+const ease      = Power3.easeOut;
 
 qmedic
     .add(createHeadingAnimFor($qmedic))
-    .fromTo($qmedic.find('.project__bg'), 0.3, {
-        yPercent: 100,
-        opacity: 0
-    }, {
-        yPercent: 0,
-        opacity: 1,
-        ease
-    })
     .fromTo($qmedic.find('.project__image'), 0.5, {
-        yPercent: 100,
+        yPercent: 50,
         opacity: 0
     }, {
         yPercent: 0,
         opacity: 1,
         ease
     })
-    .add(createAboutAnimFor($qmedic), '-=0.2');
+    .fromTo($qmedic.find('.project__bg'), 0.5, {
+        yPercent: 100,
+        opacity: 0
+    }, {
+        yPercent: 0,
+        opacity: 1,
+        ease
+    }, '-=0.2')
+    .add(createAboutAnimFor($qmedic), '-=0.5');
 
 milkyway
     .add(createHeadingAnimFor($milkyway))
-    .fromTo($milkyway.find('.bg-milkyway__layer-2'), 0.3, {
-        yPercent: 100,
-        opacity: 0
-    }, {
-        yPercent: 0,
-        opacity: 1,
-        ease
-    })
     .add([
         TweenMax.fromTo($milkyway.find('.milkyway'), 0.6, {
             yPercent: 150,
@@ -60,18 +52,16 @@ milkyway
             opacity: 0
         }, {
             x: 0,
-            delay: 0.5,
+            delay: 0.3,
             opacity: 1,
             ease
         }),
-        createAboutAnimFor($milkyway)
-    ])
-    .add([
         TweenMax.fromTo($milkyway.find('.bg-milkyway__layer-1'), 0.3, {
             xPercent: 100,
             opacity: 0
         }, {
             xPercent: 0,
+            delay: 0.3,
             opacity: 1,
             ease
         }),
@@ -80,23 +70,30 @@ milkyway
             opacity: 0
         }, {
             yPercent: 0,
+            delay: 0.5,
             opacity: 1,
             ease
         })
-    ], '-=0.1');
+    ])
+    .add([
+        TweenMax.fromTo($milkyway.find('.bg-milkyway__layer-2'), 0.4, {
+            yPercent: 100,
+            opacity: 0
+        }, {
+            yPercent: 0,
+            opacity: 1,
+            ease
+        }),
+        createAboutAnimFor($milkyway)
+    ], '-=0.3');
 
 rexpro
     .add(createHeadingAnimFor($rexpro))
-    .fromTo($rexpro.find('.bg-rex-pro__layer-2'), 0.4, {
+    .addLabel('begin', 0)
+    .fromTo($rexpro.find('.bg-rex-pro__layer-2'), 0.6, {
         xPercent: -100
     }, {
         xPercent: 0,
-        ease
-    })
-    .fromTo($rexpro.find('.bg-rex-pro__layer-3'), 0.3, {
-        opacity: 0
-    }, {
-        opacity: 1,
         ease
     })
     .fromTo($rexpro.find('.project__image'), 0.5, {
@@ -106,8 +103,14 @@ rexpro
         yPercent: 0,
         opacity: 1,
         ease
-    }, '-=0.3')
-    .add(createAboutAnimFor($rexpro), '-=0.2');
+    }, 'begin+=0.2')
+    .fromTo($rexpro.find('.bg-rex-pro__layer-3'), 0.6, {
+        opacity: 0
+    }, {
+        opacity: 1,
+        ease
+    }, 'begin+=0.5')
+    .add(createAboutAnimFor($rexpro), 'begin+=0.5');
 
 // return all Timelines include Timeline from ./intro.js
 export const animations = {
